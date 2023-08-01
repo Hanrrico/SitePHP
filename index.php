6289f4b623c4a50aec50d193ecec56c9b78f6c93
hanrrico <116649343+Hanrrico@users.noreply.github.com>
2023-08-01 14:33:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!"; // o "Echo" faz aparecer algo na tela
+$turma2 = "Vamos desenvolver website em PHP";
+echo "<br>".$turma2;
+$x = 5 /* + 15 */ + 5;
+echo "<br>".$x;
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo "<br>".$txt." x = ".$x." y =".$y
+?>
+
+</body>
+</html>
